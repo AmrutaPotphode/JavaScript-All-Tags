abccf8544c8674f4587152cd3a1c55e889da07ac
AmrutaPotphode <amrutapotphode001@gmail.com>
2025-09-18 18:18:55 +0530

    dom2

---
+++ dom.js
@@ -0,0 +1,10 @@
+const colorchange= document.getElementById("my-h1");
+
+function changecolor(){
+    colorchange.style.color="yellow";
+}
+
+const changetext=document.getElementById("para");
+function changename(){
+    changetext.textContent="Welcome Amruta";
+}
