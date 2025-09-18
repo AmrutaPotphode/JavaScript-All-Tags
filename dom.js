9e503fd3c95288046256d407b478766a21753c8d
Amruta Potphode <127823409+AmrutaPotphode@users.noreply.github.com>
2025-09-18 19:30:10 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,10 +1,33 @@
-const colorchange= document.getElementById("my-h1");
+// dom.js
 
-function changecolor(){
-    colorchange.style.color="yellow";
+// Selecting element by ID
+const heading = document.getElementById("my-h1");
+console.log(heading.innerHTML);
+
+function changeContent() {
+    const text = prompt("Add the content");
+    heading.innerHTML = text;
 }
 
-const changetext=document.getElementById("para");
-function changename(){
-    changetext.textContent="Welcome Amruta";
+// Selecting element by ClassName
+const heading2 = document.getElementsByClassName("my-h2");
+console.log(heading2[0].innerHTML);
+
+// Selecting by tagName
+const allHeadings = document.getElementsByTagName("h1");
+console.log("Total h1s:", allHeadings.length);
+
+// querySelector (selects first match)
+
+// querySelectorAll (selects all matches)
+const allH2 = document.querySelectorAll(".my-h2");
+console.log("QuerySelectorAll:", allH2);
+
+// Show and Hide Functions
+function showH1() {
+    heading.style.display = "block";
+}
+
+function hideH1() {
+    heading.style.display = "none";
 }
---
+++ script.js
@@ -0,0 +1,17 @@
+// title
+document.title
+//to change title
+document.title = "New Title"
+
+//head
+document.head
+//to change head
+document.head.innerHTML = "<h1>Heading</h1>"
+
+//body
+document.body
+//to change body
+document.body.innerHTML = "<p>Paragraph</p>"
+
+//html
+document.documentElement
